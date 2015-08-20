95ade320049b08a2caaff17b4fc016e29bfb9b3a
kylecorry31 <kylecorry31@gmail.com>
2015-08-20 12:43:48 -0400

    Add cannon subsystem

---
+++ src/org/usfirst/frc/team5112/robot/subsystems/Cannon.java
@@ -0,0 +1,17 @@
+package org.usfirst.frc.team5112.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Cannon extends Subsystem{
+
+	@Override
+	protected void initDefaultCommand() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void fire(){
+		System.out.println("Boom");
+	}
+
+}
